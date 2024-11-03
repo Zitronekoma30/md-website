a92bd046ee23c1e693b59e0d0f982fbed404af76
Leon A. <69749337+Zitronekoma30@users.noreply.github.com>
2024-11-03 13:19:33 +0100

    updates js

--- pages/fewo.js
+++ pages/fewo.js
@@ -1,34 +1,33 @@
 function showBookingModal() {
-    document.getElementById('booking-modal').style.display = 'flex';
-  }
-  
-  function hideBookingModal() {
-    document.getElementById('booking-modal').style.display = 'none';
-  }
-  
-  document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('modal')) {
-      hideBookingModal();
-    }
+  document.getElementById('booking-modal').style.display = 'flex';
+}
 
-    document.getElementById('booking-form').addEventListener('submit', function (e) {
-        e.preventDefault(); // Prevent the default form submission
-        
-        const formData = new FormData(this);
-        
-        fetch('/submit-booking', {
-          method: 'POST',
-          body: formData
-        })
-        .then(response => response.json()) // Expecting JSON response
-        .then(data => {
-          alert('Booking request sent!'); // Display success message
-          hideBookingModal(); // Hide the modal after submission
-        })
-        .catch(error => {
-          alert('Error: Unable to send booking request.'); // Display error message
-          console.error(error);
-        });
-      });      
-  });
+function hideBookingModal() {
+  document.getElementById('booking-modal').style.display = 'none';
+}
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal')) {
+    hideBookingModal();
+  }
+});
+
+document.getElementById('booking-form').addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent the default form submission
   
+  const formData = new FormData(this);
+  
+  fetch('/submit-booking', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json()) // Expecting JSON response
+  .then(data => {
+    alert('Booking request sent!'); // Display success message
+    hideBookingModal(); // Hide the modal after submission
+  })
+  .catch(error => {
+    alert('Error: Unable to send booking request.'); // Display error message
+    console.error(error);
+  });
+});
